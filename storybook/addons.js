b3dc559da18c409a5d330d91fbb1cb8330aee85b
Kishan Vaghela <kishan.j.vaghela@gmail.com>
2017-03-16 19:16:36 +0530

    add story for UserItem

--- storybook/addons.js
+++ storybook/addons.js
@@ -1,1 +1,2 @@
 import '@kadira/react-native-storybook/addons';
+import '@kadira/storybook-addon-knobs/register'
--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text } from 'react-native';
 import { storiesOf, action, linkTo } from '@kadira/react-native-storybook';
+import { withKnobs, text, boolean, number } from '@kadira/storybook-addon-knobs';
 
 import Button from './Button';
 import CenterView from './CenterView';
@@ -13,15 +14,16 @@ storiesOf('Welcome', module)
   ));
 
 storiesOf('User Item', module)
+  .addDecorator(withKnobs)
   .add('dummy text', () => {
     const budge = {
-      bronze:123,
-      silver:45,
-      gold:70
+      bronze: number('bronze', 10),
+      silver: number('silver', 20),
+      gold: number('gold', 30)
     };
     const user = {
-      display_name: "Jon Skeet",
-      reputation: "123454566",
+      display_name: text('Name', 'Arunoda Susiripala'),
+      reputation: number('Reputation', 89),
       badge_counts: budge,
       profile_image: "https://www.gravatar.com/avatar/89927e2f4bde24991649b353a37678b9?s=128&d=identicon&r=PG"
     };
