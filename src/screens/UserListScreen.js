b5a8f6e42880cd8d72127b3dcf5878d4d01dbeeb
Kishan Vaghela <kishan.j.vaghela@gmail.com>
2017-04-03 19:02:15 +0530

    add menu click event in toolbar

--- src/screens/UserListScreen.js
+++ src/screens/UserListScreen.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { TouchableHighlight, View, Text, Image, Button } from 'react-native';
 import { connect } from 'react-redux';
+import { Toolbar, ThemeProvider } from 'react-native-material-ui';
 import { fetchData } from '../actions/actions';
 import UserList from '../component/UserList';
 import ImageButton from '../component/common/ImageButton';
+
 
 class UserListScreen extends Component {
 
@@ -12,8 +14,22 @@ class UserListScreen extends Component {
   }
 
   render() {
+    const {navigate} = this.props.navigation;
     return (
       <View>
+          <ThemeProvider>
+                <Toolbar
+                 leftElement="menu"
+                 centerElement="Users"
+                 onLeftElementPress= {()=>{
+                   navigate('DrawerOpen');
+                 }}
+                 searchable={{
+                   autoFocus: true,
+                   placeholder: 'Search',
+                 }}
+               />
+          </ThemeProvider>
           {
             this.props.appData.isFetching && <Text>Loading</Text>
           }
@@ -28,16 +44,18 @@ class UserListScreen extends Component {
 }
 
 UserListScreen.navigationOptions = {
+
   header: (navigation, defaultHeader) => ({
     ...defaultHeader,
-    right: <View style={{ flexDirection: 'row'}}>
-              <ImageButton
-                source={require('../icons/ic_search_white.png')}
-                onPress={() => console.log("search")} />
-              <ImageButton
-                  source={require('../icons/ic_filter_list_white.png')}
-                  onPress={() => console.log("filter")} />
-            </View>,
+    visible: false,
+    // right: <View style={{ flex:1 ,flexDirection: 'row'}}>
+    //           <ImageButton
+    //             source={require('../icons/ic_search_white.png')}
+    //             onPress={() => console.log("search")} />
+    //           <ImageButton
+    //               source={require('../icons/ic_filter_list_white.png')}
+    //               onPress={() => console.log("filter")} />
+    //         </View>,
   }),
 };
 
