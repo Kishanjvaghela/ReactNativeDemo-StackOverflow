9ed80d84b1515c034cdcbf996f86d8645101171f
Kishan Vaghela <kishan.j.vaghela@gmail.com>
2017-04-04 12:31:45 +0530

    add toolbar in user detail screen

--- src/screens/UserDetailScreen.js
+++ src/screens/UserDetailScreen.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text,Button } from 'react-native';
+import { NavigationActions } from 'react-navigation';
+import { Toolbar, ThemeProvider } from 'react-native-material-ui';
 
 export default class UserDetailScreen extends Component {
 
   render(){
     return(
       <View>
+      <ThemeProvider>
+            <Toolbar
+            leftElement="arrow-back"
+            centerElement="User Detail"
+            onLeftElementPress= {()=> {
+              console.log("on left press");
+              this.props.navigation.dispatch(NavigationActions.back());
+             }}
+           />
+      </ThemeProvider>
         <Text>
           UserDetailScreen
         </Text>
